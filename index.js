2d0e527d3da4464007e5237867a2eb77bf055351
SheruMahedihasan <sherumahedihasan@gmail.com>
2024-04-29 12:22:40 +0530

    change

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // const fs = require('fs');
 // fs.writeFileSync('hello.php','how are you');
 const http = require('http');
+const colors = require('colors');
+// const chalk = require('chalk');
 
 http.createServer((req,res)=>{
-    // console.log('server start');
+    console.log('server start'.red);
+    // console.log(chalk.blue('Hello Chalk'));
     res.write('Hello, I am Mahedi Hasan');
     res.end();
 }).listen(4000);
