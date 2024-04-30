7d8d3e2e0a088e51ecd737331f8ae5c27ea53117
SheruMahedihasan <sherumahedihasan@gmail.com>
2024-04-30 11:43:17 +0530

    solve error

--- index.js
+++ index.js
@@ -2,11 +2,9 @@
 // fs.writeFileSync('hello.php','how are you');
 const http = require('http');
 const colors = require('colors');
-// const chalk = require('chalk');
 
 http.createServer((req,res)=>{
     console.log('server start'.red);
-    // console.log(chalk.blue('Hello Chalk'));
     res.write('Hello, I am Mahedi Hasan');
     res.end();
 }).listen(4000);
